b2a4abfd04d945ea314c0afb00fe997f2c2116ab
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-08-17 21:45:27 +0200

    add(ast): tree structure

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -1,11 +1,58 @@
 package ast
 
 import (
-    "fmt"
+	"fmt"
 	l "tomiis4/compigo/lexer"
 )
 
-func Ast(tokens []l.Token) {
-    fmt.Println("Hello, world")
-    fmt.Println(tokens)
+type Declaration struct {
+	Type       string
+	Identifier string
+	Assignment string
 }
+
+type Statement struct {
+	Declarations Declaration
+}
+
+type Block struct {
+	Statements []Statement
+}
+
+type Function struct {
+	Name   string
+	Return string
+	Block  Block
+}
+
+type Tree struct {
+	// imports
+	// declarations
+	Functions []Function
+}
+
+func Ast(tokens []l.Token) {
+	fmt.Println("Hello, world")
+	fmt.Println(tokens)
+}
+
+// ast.Program{
+//         Functions: []ast.FunctionDeclaration{
+//             {
+//                 Name:       "main",
+//                 ReturnType: "int",
+//                 Block: ast.Block{
+//                     Statements: []ast.Statement{
+//                         ast.VariableDeclaration{
+//                             Type:       "int",
+//                             Identifier: "i",
+//                         },
+//                         ast.Assignment{
+//                             Left:  ast.VariableReference{Name: "i"},
+//                             Right: ast.IntegerLiteral{Value: 5},
+//                         },
+//                     },
+//                 },
+//             },
+//         },
+//     }
